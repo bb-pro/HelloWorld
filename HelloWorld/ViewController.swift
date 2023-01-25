a2e43f0b8005b3bbe2d6c775e7e90fc360f37ee9
bektemur-07 <104037405+bektemur-07@users.noreply.github.com>
2023-01-25 22:26:52 +0500

    Initial Commit

---
+++ HelloWorld/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by Bektemur on 25/01/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
